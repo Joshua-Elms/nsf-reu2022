0b8d199b9473b99a84efef0b6112419503b83d35
Joshua-Elms <bacon.joshy@gmail.com>
2022-06-13 15:45:54 -0400

    added synth data

--- continuous_authentication/feature_extraction/experimental.py
+++ continuous_authentication/feature_extraction/experimental.py
@@ -2,14 +2,16 @@ from pathlib import PurePath
 import os
 
 def test_main():
-    c2_path = PurePath("../../data/clarkson2_files")
-    subject_84500_path_partial = PurePath("84500")
-    subject_84500_path = PurePath(c2_path, subject_84500_path_partial)
+    # c2_path = PurePath("../../data/clarkson2_files")
+    # subject_84500_path_partial = PurePath("84500")
+    sample_path = PurePath("../../data/synthetic_c2.txt")
 
-    with open(subject_84500_path, "r") as f: 
+    with open(sample_path, "r") as f: 
         rm_newline = lambda x: (x[0], x[1], x[2][:-1])
         nested_keystrokes = tuple(rm_newline(line.split("\t")) for line in f.readlines())
-        print(nested_keystrokes[:5])
+    
+    for i, event in enumerate(nested_keystrokes):
+
 
             
 
