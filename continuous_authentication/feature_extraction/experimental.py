b0a8852db82734ba13a2e57a7411b4163eb65e41
Joshua-Elms <bacon.joshy@gmail.com>
2022-06-14 16:23:09 -0400

    Meat of the preprocessing is done

--- continuous_authentication/feature_extraction/experimental.py
+++ continuous_authentication/feature_extraction/experimental.py
@@ -1,6 +1,6 @@
 from pathlib import PurePath
 import os
-import numpy as np
+
 
 def not_first_or_last(i, sample):
     return not ( (i == 0) or (i == len(sample)-1) )
@@ -14,8 +14,7 @@ def parse_word(word_indices, raw_keystrokes):
     letter_counter = [0 for _ in range(26)]
     order_dict = {}
     for i, line in enumerate(lines):
-        timestamp_str, release_str, letter = [line[i] for i in range(3)]
-        timestamp, release = int(timestamp_str), int(release_str)
+        timestamp, release, letter = [line[i] for i in range(3)]
         letter_idx = ord(letter) - 65
 
         if not release: # key was pressed
@@ -23,21 +22,51 @@ def parse_word(word_indices, raw_keystrokes):
             order_dict[f"{letter}{letter_counter[letter_idx]}"] = [lines[i]] # [i] for index instead of value
 
         else: # key not pressed
-            order_dict[f"{letter}{letter_counter[letter_idx]}"] += [(lines[i])] # [i] same
+            order_dict[f"{letter}{letter_counter[letter_idx]}"] += [lines[i]] # [i] same
 
+    dict_keys = [key for key in order_dict.keys()]
+    num_letters = len(dict_keys)
+    digraphs = [] 
+    monographs = []
+    for i, key in enumerate(dict_keys):
+        this_letter = order_dict[key]
+        this_d, this_u = this_letter
+        mono = this_u[0] - this_d[0]
+        monographs.append(mono)
+
+        if i < num_letters - 1:
+            next_letter = order_dict[dict_keys[i + 1]]
+            next_d, next_u = next_letter
+            digraphs.append(next_d[0] - this_d[0]) # dd
+            digraphs.append(next_u[0] - this_d[0]) # du
+            digraphs.append(next_d[0] - this_u[0]) # ud
+            digraphs.append(next_u[0] - this_u[0]) # uu
         
-    return order_dict
+    word = "".join([key[0] for key in dict_keys])
 
-def create_json():
-    x = {
-        'time_stamp': None,
-        'up_or_down': None,
-        'Letter': None
-    }
-    data = []
+    all_graphs = monographs + digraphs
+    timestamp_first_keypress = lines[0][0]
+    word_info = (word, timestamp_first_keypress, all_graphs)
+
+    return word_info
+
+def gen_graph_names(word):
+    # count letters first
+    letter_counter = [0 for _ in range(26)]
+    subscripted_word = []
+    for letter in word:
+        letter_idx = ord(letter) - 65
+        letter_counter[letter_idx] += 1
+        subscripted_word += [f"{letter}{letter_counter[letter_idx]}"]
+
+    monograph_labels = [f"m_{letter}" for letter in subscripted_word]
+    digraph_labels_incomplete = [f"{subscripted_word[i]}{subscripted_word[i+1]}" for i in range(len(word) - 1)]
+    digraph_labels = [f"{pre}_{post}" for post in digraph_labels_incomplete for pre in ["DD", "DU", "UD", "UU"]]
+    col_names = monograph_labels + digraph_labels
+
+    return col_names
 
 
-    print(x)
 def test_main():
     # c2_path = PurePath("../../data/clarkson2_files")
     # subject_84500_path_partial = PurePath("84500")
@@ -47,17 +76,30 @@ def test_main():
     word_cnt = 0
 
     with open(sample_path, "r") as f: 
-        rm_newline = lambda x: (x[0], x[1], x[2].rstrip("\n"))
+        rm_newline = lambda x: (int(x[0]), int(x[1]), x[2].rstrip("\n"))
         nested_keystrokes = tuple(rm_newline(line.split("\t")) for line in f.readlines())
     
     # [print(nested_keystrokes[i]) for i in range(10)]
 
+    # indices_of_all_words = find_words()
+    indices_of_all_words = [[1, 3, 4, 5, 6, 8], [i for i in range(11, 32)] + [33]]
+
+    output_dict = {}
+    for single_word_indices in indices_of_all_words:
+        word, time, graphs = parse_word(single_word_indices, nested_keystrokes)
+        output_dict[word] = [time, graphs]
+
+    print(output_dict)
+    for key, val in output_dict.items():
+        print(len(val[1]))
+        
+
     # # testing "cat"
     # parsed = parse_word([1, 3, 4, 5, 6, 8], nested_keystrokes)
 
-    # testing "addressable"
-    parsed = parse_word([i for i in range(11, 32)] + [33], nested_keystrokes)
-    print(parsed)
+    # # testing "addressable"
+    # parsed = parse_word([i for i in range(11, 32)] + [33], nested_keystrokes)
+    # print(parsed)
     # [print(line) for line in parsed]
     pass
 
