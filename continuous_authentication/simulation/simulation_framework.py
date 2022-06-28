4a2ed9e42b8cc01f8a1e39108ce610122be93604
Joshua-Elms <bacon.joshy@gmail.com>
2022-06-28 12:13:28 -0400

    Success retrieving training data

--- continuous_authentication/simulation/simulation_framework.py
+++ continuous_authentication/simulation/simulation_framework.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pathlib import PurePath
 import os
+import csv
 import sys
 sys.path.append("../../")
 
@@ -22,11 +23,24 @@ def get_train(user_arr: np.ndarray, test_digraphs: int) -> tuple:
             break
 
     random_start_position = rng.choice(np.arange(stop_pos))
-    print(f"With {eof} words, start was selected at {random_start_position}")
+    # print(f"With {eof} words, start was selected at {random_start_position}")
 
-    return random_start_position
+    t_lower, t_upper = random_start_position, random_start_position + test_digraphs
+    train = user_arr[t_lower:t_upper+1]
+    remainder = np.concatenate((user_arr[:t_lower], user_arr[(t_upper + 1):]))
 
-def main(dd):
+    return train, remainder
+
+def digraphs_in_file(file):
+    input_file = open(file,"r+")
+    reader_file = csv.reader(input_file, delimiter = "\t")
+    csv_ = list(reader_file)
+    all_digraphs = [int(csv_[i][2]) for i in range(len(csv_))]
+    sum_of_digraphs = sum(all_digraphs)
+
+    return sum_of_digraphs
+
+def main(dd, train_size, test_size):
     # Data import
     default_raw_path = PurePath("../../data/clarkson2_files/")
     default_timeseries_path = PurePath("../../data/user_time_series/")
@@ -35,11 +49,22 @@ def main(dd):
     read_paths = [path(user) for user in user_list]
 
     list_of_user_arrays = [np.genfromtxt(path, dtype = np.dtype(object), delimiter = "\t") for path in read_paths]
-
+    gahhh = "- " * 30
     # For each user, perform cross validation
     for i, user_arr in enumerate(list_of_user_arrays):
+        num_digraphs_in_file = digraphs_in_file(read_paths[i])
+
+        if num_digraphs_in_file < train_size + test_size:
+            print(gahhh)
+            print(f"User {user_list[i]} has too few digraphs: {num_digraphs_in_file}")
+            print(gahhh)
+            continue 
+
+        # print(num_digraphs_in_file)
         # Pull out training data
-        train, remainder = get_train(user_arr, test_digraphs = dd), None
+        train, remainder = get_train(user_arr, test_digraphs = dd)
+        print(f"Sample of train: {train[0]}")
+        print(f"Sample of remainder: {remainder[0]}")
 
         # Generate arrays
         # arrays_by_folds = [user_arr[fold] for fold in folds]
@@ -50,4 +75,4 @@ def main(dd):
     pass
 
 if __name__ == "__main__":
-    main(dd = 100)
+    main(dd = 100, train_size = 10000, test_size = 1000)
